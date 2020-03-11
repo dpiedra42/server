a9dbb36e24d205420c33b66dfadbefb1154ff1f3
Deanna PIEDRA <dpiedra@student.42.fr>
2020-03-11 16:22:39 +0100

    created out website directory

--- srcs/start.sh
+++ srcs/start.sh
@@ -6,13 +6,16 @@
 #    By: dpiedra <dpiedra@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/11 11:07:47 by dpiedra           #+#    #+#              #
-#    Updated: 2020/03/11 15:06:46 by dpiedra          ###   ########.fr        #
+#    Updated: 2020/03/11 16:22:23 by dpiedra          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 service nginx start
 service mysql start
 
-#configure access
+#change permissions 
 chown -R www-data /var/www
 chmod -R 755 /var/www
+
+#website directory
+mkdir /var/www/mywebsite
