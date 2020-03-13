6e15e14660bfaae499820ae6d60fdda8bae866de
deanna <42.dpiedra@gmail.com>
2020-03-13 19:13:23 +0100

    new nginx config file and phpinfo question

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    start.sh                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: dpiedra <dpiedra@student.42.fr>            +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/03/11 11:07:47 by dpiedra           #+#    #+#              #
-#    Updated: 2020/03/11 17:21:40 by dpiedra          ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 #initializing
 service nginx start
 service mysql start
@@ -21,6 +9,7 @@ chmod -R 755 /var/www
 #website directory
 mkdir /var/www/mysite
 touch /var/www/mysite/index.php
+# figure out how to put <?php phpinfo(); ?> into index
 
 #setup Nginx 
 
