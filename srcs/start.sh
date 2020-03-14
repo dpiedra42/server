08acb83059af6e0e6192952b2b2c53e46bb2d3f6
deanna <42.dpiedra@gmail.com>
2020-03-14 19:04:13 +0100

    finished nginx config and SSL certificate

--- srcs/start.sh
+++ srcs/start.sh
@@ -2,14 +2,20 @@
 service nginx start
 service mysql start
 
+#website directory
+mkdir /var/www/mysite
+
 #change permissions 
 chown -R www-data /var/www
 chmod -R 755 /var/www
 
-#website directory
-mkdir /var/www/mysite
-touch /var/www/mysite/index.php
-# figure out how to put <?php phpinfo(); ?> into index
+#SSL
+mkdir /etc/nginx/ssl
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/mysite.pem -keyout /etc/nginx/ssl/mysite.key -subj openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/monsupersite.pem -keyout /etc/nginx/ssl/monsupersite.key -subj "/C=FR/ST=Paris/L=Paris/O=42/OU=dpiedra/CN=mysite"
 
 #setup Nginx 
+mv /tmp/nginx.conf etc/nginx/sites-available/mysite
+cp etc/nginx/sites-available/mysite /etc/nginx/sites-enabled/mysite
 
+#restart
+service nginx restart
