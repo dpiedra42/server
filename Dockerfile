10550715b103e2f135f2ce299abe76b25f55b613
deanna <42.dpiedra@gmail.com>
2020-03-15 16:12:03 +0100

    trying to fix nginx

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,9 @@ LABEL maintainer="dpiedra@student.42.fr"
 RUN apt-get update
 RUN apt-get install -y wget
 RUN apt-get install -y nginx
+RUN apt-get install -y openssl
 RUN apt-get install -y mariadb-server
-RUN apt-get install -y php7.3 php7.3-mysql php7.3-fpm php7.3-common php7.3-curl php7.3 php7.3-mbstring php7.3-xml php7.3-xmlrpc php7.3-gd
+RUN apt-get install -y php7.3 php7.3-mysql php7.3-fpm php7.3-common php7.3-curl php7.3 php7.3-mbstring php7.3-xml php7.3-xmlrpc php7.3-gd php7.3-cli
 
 COPY ./srcs/start.sh/ ./
 COPY ./srcs/nginx-conf.conf ./tmp/nginx-conf.conf
--- srcs/start.sh
+++ srcs/start.sh
@@ -9,8 +9,8 @@ chown -R www-data /var/www
 chmod -R 755 /var/www
 
 #SSL (self-signed)
-mkdir /etc/nginx/ssl
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/mysite.pem -keyout /etc/nginx/ssl/mysite.key -subj openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/monsupersite.pem -keyout /etc/nginx/ssl/monsupersite.key -subj "/C=FR/ST=Paris/L=Paris/O=42/OU=dpiedra/CN=mysite"
+mkdir -p /etc/nginx/ssl
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/mysite.pem -keyout /etc/nginx/ssl/mysite.key -subj openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/monsupersite.pem -keyout /etc/nginx/ssl/monsupersite.key -subj "/C=FR/ST=Paris/L=Paris/O=Ecole 42/OU=dpiedra/CN=www.mysite.com"
 
 #setup Nginx 
 service nginx start
